7245afe194fbbe3bd1d546da15792b421d5b0a80
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-03-14 17:03:14 +0530

    project codes completed, app running as expected

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import data from './data';
+import List from './List';
 
 function App() {
   const [people, setPeople] = useState(data);
   return (
-    <div className="App">
+    <main>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
-        
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
-    </div>
+    </main>
   );
 }
 
